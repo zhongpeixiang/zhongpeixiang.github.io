c6b23f63c532e568c59e1ae4832dfd0b0d4695ca
zhongpeixiang <zhongpeixiang@gmail.com>
2020-08-13 21:59:55 +0800

    updated ngrok url

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 var userPrompt = '';
 var botPrompt = '[[b;#333;transparent]Echo][[;#333;transparent] ·] ';
-var url = 'https://8feac5e65392.ngrok.io/chat';
+var url = 'https://18918138a629.ngrok.io/chat';
 
 set_size();
 
